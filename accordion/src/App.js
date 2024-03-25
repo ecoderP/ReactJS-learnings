bb9f6342677044346e684292d2861fcb35e94bc1
Paul Onyebuchi <70877999+ecoderP@users.noreply.github.com>
2024-03-25 01:08:22 +0100

    new updates on accordion

--- accordion/src/App.js
+++ accordion/src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 
 
 const faqs = [
@@ -29,16 +30,39 @@ function App() {
 }
 
 
-function Accordion({ num, title, text}) {
+function Accordion({ data }) {
 
   return (
     <div className="accordion">
-      <item />
+      {data.map((el, i) => (
+        <AccordionItem title={el.title} text={el.text} num={i} key={el.title} />
+      ))}
     </div>
   )
 }
 
 
+function AccordionItem({ num, title, text }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleToggle() {
+    setIsOpen((isOpen) => !isOpen);
+  }
+
+
+  return (
+    <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
+      <p className="number">
+        {num < 10 ? `0${num + 1}` : num + 1}
+      </p>
+      <p className="text">{title}</p>
+      <p className="icon">{isOpen ? '-' : '+'}</p>
+      { isOpen && <div className="content-box">{text}</div> }
+    </div>
+  )
+}
+
+ 
 
 
 export default App;
